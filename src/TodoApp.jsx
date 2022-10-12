83f3c9e54b17bc9e5583ddbc6446f5c7ae0a6e70
Ran Kirlian <rankirlian@gmail.com>
2022-10-12 23:52:42 +0200

    Update TodoApp to add basic structure and layout

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,20 +1,45 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './TodoApp.css'
 
+import { todoReducer } from './components/todoReducer';
+
+import './TodoApp.css';
+
+const initialState = [
+  {
+    id: new Date().getTime(),
+    todo: "create useReducer example",
+    done: false
+  }
+];
 
 function TodoApp() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(todoReducer, initialState)
 
   return (
-    <div className="TodoApp">
+    <div className="container">
       <h1>TodoApp</h1>
       <hr />
-      <ul>
-        <li>Uno</li>
-        <li>Dos</li>
-        <li>Tres</li>
-      </ul>
+      <div className='row'>
+        <div className='col-7'>
+          <ul>
+            <li>Uno</li>
+            <li>Dos</li>
+            <li>Tres</li>
+          </ul>
+        </div>
+        <div className='col-5'>
+          <h4>Add TODO</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder='Add some task to do'
+              className='form-control'
+            />
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
