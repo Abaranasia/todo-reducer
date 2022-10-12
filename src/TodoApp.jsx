788fcaa5a02ea4e8fc9dad30c882b644b8fc686c
Ran Kirlian <rankirlian@gmail.com>
2022-10-13 00:09:01 +0200

    Update TodoApp to extend basic layout and add Bootstrap classes

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 
 import { todoReducer } from './components/todoReducer';
 
-import './TodoApp.css';
+// import './TodoApp.css';
 
 const initialState = [
   {
@@ -18,14 +18,15 @@ function TodoApp() {
 
   return (
     <div className="container">
-      <h1>TodoApp</h1>
+      <h1>TodoApp (10), <small>pending: 2</small></h1>
       <hr />
       <div className='row'>
         <div className='col-7'>
-          <ul>
-            <li>Uno</li>
-            <li>Dos</li>
-            <li>Tres</li>
+          <ul className='list-group'>
+            <li className='list-group-item d-flex justify-content-between'>
+              <span className='align-self-center'>Uno</span>
+              <button className='btn btn-danger'>Delete</button>
+            </li>
           </ul>
         </div>
         <div className='col-5'>
@@ -37,6 +38,12 @@ function TodoApp() {
               placeholder='Add some task to do'
               className='form-control'
             />
+            <button
+              type="submit"
+              className='btn btn-outline-primary mt-1'
+            >
+              Add
+            </button>
           </form>
         </div>
       </div>
