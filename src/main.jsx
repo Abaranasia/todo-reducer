593ad7cf0260813d64dd06a1bce8f5d2cea7f06e
Ran Kirlian <rankirlian@gmail.com>
2022-10-13 00:08:31 +0200

    Update main to comment styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import TodoApp from './TodoApp'
-import './index.css'
+
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
