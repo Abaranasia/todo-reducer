139ae4c283b3c13f81490ac0f674c87155c5172a
Ran Kirlian <rankirlian@gmail.com>
2022-10-15 11:47:34 +0200

    Update TodoItem to add handle delete button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { handleDeleteAction } from '../actions/todoActions';
+
+const handleDeleteButton = (todoId) => {
+  handleDeleteAction(todoId)
+};
 
 export const TodoItem = ({ todo }) => {
   return (
     <li className='list-group-item d-flex justify-content-between'>
       <span className='align-self-center'>{todo.todo}</span>
-      <button className='btn btn-danger'>Delete</button>
+      <button
+        className='btn btn-danger'
+        onClick={() => handleDeleteButton(todo.id)}
+      >
+        Delete
+      </button>
+
     </li>
   )
 }
