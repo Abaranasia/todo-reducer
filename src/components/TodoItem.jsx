b3346e53d374da70d0a83ede9d2856e19698578b
Ran Kirlian <rankirlian@gmail.com>
2022-10-17 23:08:21 +0200

    Update TodoItem to call delete dispatcher

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import { handleDeleteAction } from '../actions/todoActions';
 
-const handleDeleteButton = (todoId) => {
-  handleDeleteAction(todoId)
-};
+export const TodoItem = ({ todo, onDelete }) => {
 
-export const TodoItem = ({ todo }) => {
   return (
     <li className='list-group-item d-flex justify-content-between'>
       <span className='align-self-center'>{todo.todo}</span>
       <button
         className='btn btn-danger'
-        onClick={() => handleDeleteButton(todo.id)}
+        onClick={() => onDelete(todo.id)}
       >
         Delete
       </button>
