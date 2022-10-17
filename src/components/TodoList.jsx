7c8a1c11c7667c8e8946f57580daec55caf6e1d0
Ran Kirlian <rankirlian@gmail.com>
2022-10-17 23:08:59 +0200

    Update TodoList to send onDelete function to TodoItem child component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { TodoItem } from './TodoItem'
 
-export const TodoList = ({ todos = [] }) => {
+export const TodoList = ({ todos = [], onDelete }) => {
   return (
     <ul className='list-group'>
       {
         todos.map(todo => (
-          <TodoItem todo={todo} key={todo.id} />
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            onDelete={onDelete}
+          />
         ))
       }
     </ul>
