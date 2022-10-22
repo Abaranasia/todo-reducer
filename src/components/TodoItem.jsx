dfec1d18f6f9116e7bdd81e077857f92bb36860f
Ran Kirlian <rankirlian@gmail.com>
2022-10-22 19:50:49 +0200

    Update TodoItem to add the toggle event handler

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export const TodoItem = ({ todo, onDelete }) => {
+export const TodoItem = ({ todo, onDelete, onToggleTodo }) => {
 
   return (
     <li className='list-group-item d-flex justify-content-between'>
-      <span className='align-self-center'>{todo.todo}</span>
+      <span
+        className={`align-self-center ${(todo.done) ? 'text-decoration-line-through' : ''}`}
+        onClick={() => onToggleTodo(todo.id)}
+      >
+        {todo.todo}
+      </span>
       <button
         className='btn btn-danger'
         onClick={() => onDelete(todo.id)}
