1fce94b094b0b76b5d277d1914fdc7fb101bf4e0
Ran Kirlian <rankirlian@gmail.com>
2022-10-22 19:52:20 +0200

    Update TodoApp to extract all action handlers to useTodos custom hook

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,60 +1,31 @@
 import { useEffect, useReducer, useState } from 'react'
 import reactLogo from './assets/react.svg'
 
-import { todoReducer } from './reducer/todoReducer';
-
 import { TodoAdd } from './components/TodoAdd';
 import { TodoList } from './components/TodoList';
-
-
-// import './TodoApp.css';
-
-const initialState = [
-  {
-    id: new Date().getTime(),
-    todo: "create useReducer example",
-    done: false
-  }
-];
-
-const init = () => {
-  return JSON.parse(localStorage.getItem('todos')) || [];
-};
+import { useTodos } from './hooks/useTodos';
 
 const TodoApp = () => {
-  const [todos, dispatch] = useReducer(todoReducer, initialState, init)
 
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos])
-
-  const handleAddAction = (newTodo) => {
-    // Sets the add action & payload to dispatch
-    const action = {
-      type: '[TODO] add todo',
-      payload: newTodo,
-    };
-    dispatch(action);
-  };
-
-  const handleDeleteAction = (id) => {
-    // Sets the delete action & payload to dispatch
-    dispatch({
-      type: '[TODO] remove todo',
-      payload: id,
-    });
-  };
-
+  const {
+    todos,
+    todosCount,
+    pendingTodos,
+    handleAddAction,
+    handleDeleteAction,
+    handleToggleAction,
+  } = useTodos();
 
   return (
     <div className="container">
-      <h1>TodoApp (10), <small>pending: 2</small></h1>
+      <h1>TodoApp ({todosCount}), <small>pending: {pendingTodos}</small></h1>
       <hr />
       <div className='row'>
         <div className='col-7'>
           <TodoList
             todos={todos}
             onDelete={handleDeleteAction}
+            onToggleTodo={handleToggleAction}
           />
         </div>
         <div className='col-5'>
@@ -65,6 +36,6 @@ const TodoApp = () => {
       </div>
     </div>
   )
-}
+};
 
 export default TodoApp
