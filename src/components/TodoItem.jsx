de1177ad0b0860d630f4855e8c9630d6ba38743d
Ran Kirlian <rankirlian@gmail.com>
2022-10-22 22:29:24 +0200

    Update TodoItem to consume handleDeleteAction and handleToggleAction from TodoContext

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TodoContext } from '../context/TodoContext'
 
-export const TodoItem = ({ todo, onDelete, onToggleTodo }) => {
+export const TodoItem = ({ todo }) => {
+
+  const { handleDeleteAction, handleToggleAction } = useContext(TodoContext);
 
   return (
     <li className='list-group-item d-flex justify-content-between'>
       <span
         className={`align-self-center ${(todo.done) ? 'text-decoration-line-through' : ''}`}
-        onClick={() => onToggleTodo(todo.id)}
+        onClick={() => handleToggleAction(todo.id)}
       >
         {todo.todo}
       </span>
       <button
         className='btn btn-danger'
-        onClick={() => onDelete(todo.id)}
+        onClick={() => handleDeleteAction(todo.id)}
       >
         Delete
       </button>
